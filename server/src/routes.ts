57e49d5fb535f79d3ab933954ad140e0e42510fa
Eduardo Feliciano <eduardofeliciano74@gmail.com>
2026-04-25 21:51:00 -0300

    Refatorando comentário de routes e nome da rota de contagem total

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,10 +9,9 @@ routes.post("/shoes", createProduct);
 routes.get("/shoes", readAllProducts);
 routes.patch("/shoes/:id", updateProduct);
 routes.delete("/shoes/:id", deleteProduct);
-// Teria como eu deixar a url mais parecida? fazer ele ser sensível ao dado?
 routes.get("/shoes/tamanho/:tamanho", getByTamanho);
 routes.get("/shoes/marca/:marca", getByMarca);
-routes.get("/total/sapatos", countOfPares);
+routes.get("/total", countOfPares);
 
 
 
